4a9fed5852c1491558fb3c3ac30aa09080f86cae
Dev.Pasaka <dev.pasaka@gmail.com>
2024-04-05 01:08:01 +0300

    first commit

--- src/main/kotlin/com/pavicontech/api/data/config/Config.kt
+++ src/main/kotlin/com/pavicontech/api/data/config/Config.kt
@@ -4,12 +4,12 @@ import io.ktor.server.engine.*
 
 
 object Config{
-    val databaseUrl = "mongodb+srv://PascarlDevDB:Pasaka02105416001@pascarlsdevdb.sorce9p.mongodb.net/?retryWrites=true&w=majority&appName=PascarlsDevDB"
+    val databaseUrl = ""
     val databaseName = "PaviconTech"
 
 
-    val USERNAME = "admin@coinx.co.ke"
-    val PASSWORD = "(-5VLeN3L5tbd7"
+    val USERNAME = ""
+    val PASSWORD = ""
     val SMTP_SERVER_ADDRESS  = "sm1.cloudoon.com"
 
     val apiVersionBranch = "api/v1/"
