8b3f72065803f12b8a018cc25d18ab2901a11b98
Dev.Pasaka <dev.pasaka@gmail.com>
2024-04-05 01:03:52 +0300

    removed secretes

--- src/main/kotlin/com/pavicontech/api/data/config/Config.kt
+++ src/main/kotlin/com/pavicontech/api/data/config/Config.kt
@@ -6,8 +6,7 @@ import io.ktor.server.engine.*
 object Config{
     val databaseUrl = "mongodb+srv://PascarlDevDB:Pasaka02105416001@pascarlsdevdb.sorce9p.mongodb.net/?retryWrites=true&w=majority&appName=PascarlsDevDB"
     val databaseName = "PaviconTech"
-    val sendGridApiKey = "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"
-    val sendGridBaseUrl = "https://api.sendgrid.com/v3/mail/send"
+
 
     val USERNAME = "admin@coinx.co.ke"
     val PASSWORD = "(-5VLeN3L5tbd7"
